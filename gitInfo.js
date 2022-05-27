f88234df09313f4a8dfda85b94588b6ed63b4286
dallasmay <dmmay9@gmail.com>
2022-05-27 11:16:25 -0600

    Completed gitInfo.js problems

--- gitInfo.js
+++ gitInfo.js
@@ -10,6 +10,7 @@
 */
 
 //CODE HERE
+let gitDefinition = "Git is a program that allows for version control. You can 'save' your code periodically and it will save your code exactly as it was when you saved it. This allows you to essentially 'go back in time' in your code to see if what was different.";
 
 //////////////////PROBLEM 2////////////////////
 /*
@@ -18,6 +19,7 @@
 */
 
 //CODE HERE
+let gitHubDefinition = "GitHub is a website that allows you to store your code repositories and commits on the cloud. This allows you to maintain access to all your code even if something happened on your local storage, like files getting corrupted or if your computer was stolen";
 
 //////////////////PROBLEMS 3 - 8////////////////////
 /*
@@ -34,7 +36,10 @@
 */
 
 //CODE HERE
-
+const init = {
+    code: "git init",
+    description: "Init creates (initializes) a repository in the directory that you're in"
+};
 //////////////////PROBLEM 4////////////////////
 /*
     Create an object called 'clone' with 'description' and 'code' properties 
@@ -42,7 +47,10 @@
 */
 
 //CODE HERE
-
+const clone = {
+    code: "git clone",
+    description: "Clone copies an entire existing Git repository and creates a repository on your computer and populates it with the clone of the existing one"
+};
 //////////////////PROBLEM 5////////////////////
 /*
     Create an object called 'status' with 'description' and 'code' properties 
@@ -50,7 +58,10 @@
 */
 
 //CODE HERE
-
+const status = {
+    code: "git status",
+    description: "Status allows you to see the status of your repository. It shows you if you've made any changes since your last commit, and shows you if you've staged any files to be ready for commit"
+};
 //////////////////PROBLEM 6////////////////////
 /*
     Create an object called 'add' with 'description' and 'code' properties 
@@ -58,7 +69,10 @@
 */
 
 //CODE HERE
-
+const add = {
+    code: "git add",
+    description: "Add allows you to stage files to be ready to commit them"
+};
 //////////////////PROBLEM 7////////////////////
 /*
     Create an object called 'commit' with 'description' and 'code' properties
@@ -66,7 +80,10 @@
 */
 
 //CODE HERE
-
+const commit = {
+    code: "git commit",
+    description: "Commit allows you to 'save' the changes you've made on a file or multiple files. You can go back into all the commits you've made on a repository and load the code"
+};
 
 //////////////////PROBLEM 8////////////////////
 /*
@@ -75,3 +92,7 @@
 */
 
 //CODE HERE
+const push = {
+    code: "git push",
+    description: "Push allows you to push your repository/commits to GitHub, so that they're saved on the cloud"
+};
