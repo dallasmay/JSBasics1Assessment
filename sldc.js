0ecba089c8758fe912dee3df42bbfd80058b6cc1
dallasmay <dmmay9@gmail.com>
2022-05-27 10:18:37 -0600

    Part 1: Software Development Lifecycle completed

--- sldc.js
+++ sldc.js
@@ -5,15 +5,21 @@
     Reorganize the following steps of the Software Development Lifecycle to be in their correct order.
 */
 
+// Planning
 // Analysis of Requirements
 // Design
 // Implementation
-// Maintain
-// Planning
 // Testing & Integration
+// Maintain
 
 
 //////////////////PROBLEM 2////////////////////
 /*
     Create a variable for each of the above steps of the Software Development Lifecycle and set the values of each variable to a definition of the step.
 */
+let planning = "Planning is the first step in the Sofware Development Life Cycle and includes stating what the application is going to be and setting goals to complete the project in a timely manner.";
+let analysisOfRequirements = "Analysis of Requirements is the step where you go a little deeper into the requirements of the project. What features are we going to need? Do those features fit into the budget of the client? How long should each part of the project take and how many engineers will be needed to complete it on time?";
+let design = "Design is where the ideas start to become something more tangible. Each part of the project will be mapped out, outlining every single feature and how the team is planning on making it.";
+let implementation = "Implementation is where the actual coding happens. All the developers actually create the project and its features";
+let testingAndIntegration = "Testing and integration has two parts. Testing is crucial as it ensures that the project is functioning properly and helps identify bugs that can be fixed. Once the parties are satisfied with how the project is functioning, you move to integration. Integration is where the project goes live. For example, a website going live and being open to the public, or making an app available to the client's customers, etc.";
+let maintain = "Ongoing mainetence of the project. Technology is always changing, and the longer software goes without being updated, the more likely it is to break and function improperly. Therefore it needs to be maintained if it needs to continue functioning";
